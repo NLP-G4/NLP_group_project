b59db9139ed1302a2a4a62dad67fa9deba403296
Tyler-Applegate <tyler.applegate@gmail.com>
2021-08-01 21:51:33 -0500

    added overview function

--- acquire.py
+++ acquire.py
@@ -232,3 +232,99 @@ def get_github_data(cached=False):
         df = pd.read_csv(filename, index_col = 0)
 
     return df
+
+def overview(df):
+    '''
+    This function returns the shape and info of the df. It also includes a breakdown of the number of unique values
+    in each column to determine which are categorical/discrete, and which are numerical/continuous. Finally, it returns
+    a breakdown of the statistics on all numerica columns.
+    '''
+    
+    header = df.head()
+    print(f'This dataframe has {df.shape[0]} rows and {df.shape[1]} columns.')
+    print('----------------------------------')
+    print('')
+    print(df.info())
+    print('----------------------------------')
+    print('')
+    print('Unique value counts of each column')
+    print('')
+    print(df.nunique())
+    print('----------------------------------')
+    print('')
+    print('Stats on Numeric Columns')
+    print('')
+    print(df.describe())
+    print('----------------------------------')
+    print('')
+    print('Target Value Counts')
+    print(df.language.value_counts())
+    
+def nulls_by_col(df):
+    '''
+    This function determines how many null values there are in each column, and returns a pandas DataFrame
+    with 3 columns, index=column name, 
+    num_missing=number of missing values per columnm, 
+    prcnt_miss=what percent of each column is null.
+    '''
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    prcnt_miss = num_missing / rows * 100
+    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': prcnt_miss})
+    return cols_missing
+
+def nulls_by_row(df):
+    '''
+    This function determines how many null values there are in each row, and returns a pandas DataFrame
+    with 4 columns, index=index, 
+    num_missing=number of missing values per row, 
+    prcnt_miss=what percent of each row is null,
+    num_rows=how many rows are missing this many values.
+    '''
+    num_missing = df.isnull().sum(axis=1)
+    prcnt_miss = num_missing / df.shape[1] * 100
+    rows_missing = pd.DataFrame({'num_cols_missing': num_missing, 'percent_cols_missing': prcnt_miss})\
+    .reset_index()\
+    .groupby(['num_cols_missing', 'percent_cols_missing']).count()\
+    .rename(index=str, columns={'index': 'num_rows'}).reset_index()
+    return rows_missing
+
+def summarize(df):
+    '''
+    This function will take in a single argument (pandas DF)
+    and output to console various statistics on said DF, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observe null values
+    '''
+    print('----------------------------------------------------')
+    print('DataFrame Head')
+    print(df.head(3))
+    print('----------------------------------------------------')
+    print('DataFrame Info')
+    print(df.info())
+    print('----------------------------------------------------')
+    print('DataFrame Description')
+    print(df.describe())
+    num_cols = [col for col in df.columns if df[col].dtype != 'O']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    print('----------------------------------------------------')
+    print('DataFrame Value Counts: ')
+    for col in df.columns:
+        if col in cat_cols:
+            print(df[col].value_counts())
+            print('--------------------------------------------')
+            print('')
+        else:
+            print(df[col].value_counts(bins=10, sort=False))
+            print('--------------------------------------------')
+            print('')
+    print('----------------------------------------------------')
+    print('Nulls in DataFrame by Column: ')
+    print(nulls_by_col(df))
+    print('----------------------------------------------------')
+    print('Nulls in DataFrame by Rows: ')
+    print(nulls_by_row(df))
+    print('----------------------------------------------------')
